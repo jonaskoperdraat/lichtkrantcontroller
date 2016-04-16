66e0a4539e931f3e2068f15c8c33ba39b33e3fd5
jonas <jonas@jonaskoperdraat.nl>
2016-04-16 13:35:30 +0200

    Added Angular. Angular-enabled the uploadform. TODO: process result Added initial static list for show and pages.

--- src/main/webapp/scripts/lichtkrantcontroller.js
+++ src/main/webapp/scripts/lichtkrantcontroller.js
@@ -1,3 +1,27 @@
+//inject angular file upload directives and services.
+var app = angular.module('lichtkrantControllerApp', ['ngFileUpload']);
+
+app.controller('lichtkrantControllerCtrl', ['$scope', 'Upload', '$timeout', function ($scope, Upload, $timeout) {
+    $scope.loadShow = function(file) {
+        file.upload = Upload.upload({
+            url: '/show/open',
+            data: {file: file},
+        });
+
+        file.upload.then(function (response) {
+            $timeout(function () {
+                file.result = response.data;
+            });
+        }, function (response) {
+            if (response.status > 0)
+                $scope.errorMsg = response.status + ': ' + response.data;
+        }, function (evt) {
+            // Math.min is to fix IE which reports 200% sometimes
+            file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+        });
+    }
+}]);
+
 $(document).on('change', '.btn-file :file', function() {
     var input = $(this),
         numFiles = input.get(0).files ? input.get(0).files.length : 1,
