8b507d6b7e3f890de2ecea96b0570ed275d248e4
jonas <jonas@jonaskoperdraat.nl>
2016-05-02 22:33:17 +0200

    pom.xml opgeruimd.

---
+++ src/test/java/testApplication.java
@@ -0,0 +1,12 @@
+/**
+ * Created by jonas on 29-4-2016.
+ */
+public class testApplication {
+
+    public static void main(String[] args) {
+        Short a = Short.parseShort("11000000", 2);
+        Byte b = Byte.parseByte("11000000", 2);
+        System.out.println(a);
+    }
+
+}
